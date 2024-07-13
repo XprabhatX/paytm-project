cdc5e218704ca8cf1f783e7fb99418bd6d329444
Prabhat Pankaj <prabhatpankajofficial@gmail.com>
2024-07-13 15:02:51 +0530

    ci test 3

--- apps/merchant-app/app/page.tsx
+++ apps/merchant-app/app/page.tsx
@@ -1,10 +1,11 @@
 "use client";
 
-import { useBalance } from "@repo/store/balance";
+// import { useBalance } from "@repo/store/balance";
 
 export default function() {
-  const balance = useBalance();
+  // const balance = useBalance();
+  const balance = 99900;
   return <div>
-    hi there {balance}
+    hi there balance: {balance}
   </div>
 }
