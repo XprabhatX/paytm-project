804c9516c22647e1ead9bac89c811d8478521f02
Prabhat Pankaj <prabhatpankajofficial@gmail.com>
2024-07-13 15:42:42 +0530

    ci pipeline test 6

--- apps/merchant-app/app/page.tsx
+++ apps/merchant-app/app/page.tsx
@@ -2,18 +2,10 @@
 
 // import { useBalance } from "@repo/store/balance";
 
-
 export default function() {
-
-
-
-  
   // const balance = useBalance();
-  const balance = 19900;
+  const balance = 99900;
   return <div>
     Hi Merchant your balance is ₹ {balance}
   </div>
-
-
-
 }
